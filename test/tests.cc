06ed1fa766b8e6c634e0bdba1355ece372c20e4a
mfurga <mfurga@protonmail.com>
2019-01-05 13:12:03 +0100

    Update tests. Add get_number_repr()

--- test/tests.cc
+++ test/tests.cc
@@ -9,7 +9,7 @@ TEST(Bignum, InitStartsEmptyNumber)
   Bignum number;
   EXPECT_EQ(number.size, 0);
   EXPECT_EQ(number.sign, POSITIVE);
-  EXPECT_EQ(number.number, std::vector<uint32_t>());
+  EXPECT_EQ(number.get_number_repr(), std::vector<uint32_t>());
 }
 
 TEST(Bignum, InitStartsNotEmptyNumberAsInt)
@@ -17,35 +17,35 @@ TEST(Bignum, InitStartsNotEmptyNumberAsInt)
   Bignum number_positive = 391283092831231;
   EXPECT_EQ(number_positive.size, 15);
   EXPECT_EQ(number_positive.sign, POSITIVE);
-  EXPECT_EQ(number_positive.number, std::vector<uint32_t>({
+  EXPECT_EQ(number_positive.get_number_repr(), std::vector<uint32_t>({
     92831231, 391283
   }));
 
   Bignum number_positive_zero = 0;
   EXPECT_EQ(number_positive_zero.size, 1);
   EXPECT_EQ(number_positive_zero.sign, POSITIVE);
-  EXPECT_EQ(number_positive_zero.number, std::vector<uint32_t>({
+  EXPECT_EQ(number_positive_zero.get_number_repr(), std::vector<uint32_t>({
     0
   }));
 
   Bignum number_negative = -1028192891009847129;
   EXPECT_EQ(number_negative.size, 19);
   EXPECT_EQ(number_negative.sign, NEGATIVE);
-  EXPECT_EQ(number_negative.number, std::vector<uint32_t>({
+  EXPECT_EQ(number_negative.get_number_repr(), std::vector<uint32_t>({
     9847129, 28192891, 1
   }));
 
   Bignum number_negative_one = -1;
   EXPECT_EQ(number_negative_one.size, 1);
   EXPECT_EQ(number_negative_one.sign, NEGATIVE);
-  EXPECT_EQ(number_negative_one.number, std::vector<uint32_t>({
+  EXPECT_EQ(number_negative_one.get_number_repr(), std::vector<uint32_t>({
     1
   }));
 
   Bignum number_positive_one = 1;
   EXPECT_EQ(number_positive_one.size, 1);
   EXPECT_EQ(number_positive_one.sign, POSITIVE);
-  EXPECT_EQ(number_positive_one.number, std::vector<uint32_t>({
+  EXPECT_EQ(number_positive_one.get_number_repr(), std::vector<uint32_t>({
     1
   }));
 }
@@ -55,7 +55,7 @@ TEST(Bignum, InitStartsNotEmptyNumberAsCString)
   Bignum number_positive = "65432787129128912839128329831938195831";
   EXPECT_EQ(number_positive.size, 38);
   EXPECT_EQ(number_positive.sign, POSITIVE);
-  EXPECT_EQ(number_positive.number, std::vector<uint32_t>({
+  EXPECT_EQ(number_positive.get_number_repr(), std::vector<uint32_t>({
     938195831, 128329831, 128912839, 432787129, 65
   }));
 
@@ -66,7 +66,7 @@ TEST(Bignum, InitStartsNotEmptyNumberAsCString)
                                 "010019201295238103183899181923103012837124012");
   EXPECT_EQ(number_positive_big.size, 45 * 5);
   EXPECT_EQ(number_positive_big.sign, POSITIVE);
-  EXPECT_EQ(number_positive_big.number, std::vector<uint32_t>({
+  EXPECT_EQ(number_positive_big.get_number_repr(), std::vector<uint32_t>({
     837124012, 923103012, 183899181, 295238103,  10019201,
     981298129, 298129182,  18273011, 301301237, 827301287,
     123101230, 238712837, 309123871, 840128312,  12901820,
@@ -77,7 +77,7 @@ TEST(Bignum, InitStartsNotEmptyNumberAsCString)
   Bignum number_negative = "-2938198349834000839100002283900000312983";
   EXPECT_EQ(number_negative.size, 40);
   EXPECT_EQ(number_negative.sign, NEGATIVE);
-  EXPECT_EQ(number_negative.number, std::vector<uint32_t>({
+  EXPECT_EQ(number_negative.get_number_repr(), std::vector<uint32_t>({
     312983, 2283900, 839100, 198349834, 2938
   }));
 
@@ -88,7 +88,7 @@ TEST(Bignum, InitStartsNotEmptyNumberAsCString)
                                  "000000000000000000000000000000000000000000000");
   EXPECT_EQ(number_negative_big.size, 45 * 5);
   EXPECT_EQ(number_negative_big.sign, NEGATIVE);
-  EXPECT_EQ(number_negative_big.number, std::vector<uint32_t>({
+  EXPECT_EQ(number_negative_big.get_number_repr(), std::vector<uint32_t>({
             0,         0,         0,         0,         0,
     491491419,  39819491,   3452411,  10230000, 219381293,
     839859170, 349123912,      4981, 230000000,      1231,
@@ -99,21 +99,21 @@ TEST(Bignum, InitStartsNotEmptyNumberAsCString)
   Bignum number_positive_zero = "0";
   EXPECT_EQ(number_positive_zero.size, 1);
   EXPECT_EQ(number_positive_zero.sign, POSITIVE);
-  EXPECT_EQ(number_positive_zero.number, std::vector<uint32_t>({
+  EXPECT_EQ(number_positive_zero.get_number_repr(), std::vector<uint32_t>({
     0
   }));
 
   Bignum number_negative_one = "-1";
   EXPECT_EQ(number_negative_one.size, 1);
   EXPECT_EQ(number_negative_one.sign, NEGATIVE);
-  EXPECT_EQ(number_negative_one.number, std::vector<uint32_t>({
+  EXPECT_EQ(number_negative_one.get_number_repr(), std::vector<uint32_t>({
     1
   }));
 
   Bignum number_positive_one = "1";
   EXPECT_EQ(number_positive_one.size, 1);
   EXPECT_EQ(number_positive_one.sign, POSITIVE);
-  EXPECT_EQ(number_positive_one.number, std::vector<uint32_t>({
+  EXPECT_EQ(number_positive_one.get_number_repr(), std::vector<uint32_t>({
     1
   }));
 }
@@ -123,35 +123,35 @@ TEST(Bignum, InitStartsNotEmptyNumberAsStdString)
   Bignum number_positive = std::string("9182981300000198219");
   EXPECT_EQ(number_positive.size, 19);
   EXPECT_EQ(number_positive.sign, POSITIVE);
-  EXPECT_EQ(number_positive.number, std::vector<uint32_t>({
+  EXPECT_EQ(number_positive.get_number_repr(), std::vector<uint32_t>({
     198219, 182981300, 9,
   }));
 
   Bignum number_negative = std::string("-1283192192891829182912");
   EXPECT_EQ(number_negative.size, 22);
   EXPECT_EQ(number_negative.sign, NEGATIVE);
-  EXPECT_EQ(number_negative.number, std::vector<uint32_t>({
+  EXPECT_EQ(number_negative.get_number_repr(), std::vector<uint32_t>({
     829182912, 192192891, 1283
   }));
 
   Bignum number_positive_zero = std::string("0");
   EXPECT_EQ(number_positive_zero.size, 1);
   EXPECT_EQ(number_positive_zero.sign, POSITIVE);
-  EXPECT_EQ(number_positive_zero.number, std::vector<uint32_t>({
+  EXPECT_EQ(number_positive_zero.get_number_repr(), std::vector<uint32_t>({
     0
   }));
 
   Bignum number_positive_one = std::string("1");
   EXPECT_EQ(number_positive_one.size, 1);
   EXPECT_EQ(number_positive_one.sign, POSITIVE);
-  EXPECT_EQ(number_positive_one.number, std::vector<uint32_t>({
+  EXPECT_EQ(number_positive_one.get_number_repr(), std::vector<uint32_t>({
     1
   }));
 
   Bignum number_negative_one = std::string("-1");
   EXPECT_EQ(number_negative_one.size, 1);
   EXPECT_EQ(number_negative_one.sign, NEGATIVE);
-  EXPECT_EQ(number_negative_one.number, std::vector<uint32_t>({
+  EXPECT_EQ(number_negative_one.get_number_repr(), std::vector<uint32_t>({
     1
   }));
 }
